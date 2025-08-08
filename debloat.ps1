8f9f871be03b301ea7276f0121a1f65998c234ca
Jason Scheirer <jscheirer@makenotion.com>
2025-08-08 12:16:32 -0700

    Corretly change start menu

--- debloat.ps1
+++ debloat.ps1
@@ -2,4 +2,4 @@
 
 Write-Host "Debloating OS"
 
-& ([scriptblock]::Create((Invoke-RestMethod "https://debloat.raphi.re/"))) -Silent -RunDefaults	-ClearStart	-HideSearchTb -DisableWidgets -TaskbarAlignLeft -ExplorerToThisPC  -DisableLockscreenTips -DisableDesktopSpotlight
+& ([scriptblock]::Create((Invoke-RestMethod "https://debloat.raphi.re/"))) -Silent -RunDefaults	-ClearStart	-HideSearchTb -DisableWidgets -TaskbarAlignLeft -ExplorerToThisPC -DisableLockscreenTips -DisableDesktopSpotlight
--- desktop_theme.ps1
+++ desktop_theme.ps1
@@ -3,10 +3,14 @@
 Write-Host "Setting desktop theme"
 
 # Light UI, dark start menu etc
-Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes" -Name "SystemUsesLightTheme" -Value 0 -Type "Dword"
-Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes" -Name "AppsUseLightTheme" -Value 1 -Type "Dword"
+Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0 -Type "Dword"
+Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 1 -Type "Dword"
+
+# Search off
+Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchBoxTaskbarMode" -Value 0 -Type "Dword"
 
 # Solid color BG
+Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers" -Name "BackgroundType" -Value 1 -Type "Dword"
 Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" -Name "Wallpaper" -Value "" -Type "String"
 Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Control Panel\Colors" -Name "Background" -Value "249 245 241" -Type "String"
 
