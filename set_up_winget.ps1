6e55838f0a412ce8e9387a9851ba2ee6db58fd8b
Jason Scheirer <jscheirer@makenotion.com>
2025-08-11 15:55:41 -0700

    Update set_up_winget.ps1

--- set_up_winget.ps1
+++ set_up_winget.ps1
@@ -4,3 +4,12 @@ $progressPreference = 'silentlyContinue'
 Install-PackageProvider -Name NuGet -Force | Out-Null
 Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
 Repair-WinGetPackageManager -AllUsers
+
+Write-Host "Enabling Dev Mode"
+$RegistryKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock"
+if (-not(Test-Path -Path $RegistryKeyPath)) {
+    New-Item -Path $RegistryKeyPath -ItemType Directory -Force
+}
+
+# Add registry value to enable Developer Mode
+New-ItemProperty -Path $RegistryKeyPath -Name AllowDevelopmentWithoutDevLicense -PropertyType DWORD -Value 1
