3a5a668845ee510a0d26138afd50b23a30dfa187
Jason Scheirer <jscheirer@makenotion.com>
2025-08-12 08:18:52 -0700

    Update sanity_check_build_env.ps1

--- sanity_check_build_env.ps1
+++ sanity_check_build_env.ps1
@@ -1,1 +1,6 @@
-# HELLO
+# Set up nvm
+$NvmVersion = "v22.13.1"
+& "$env:LOCALAPPDATA\nvm\nvm.exe" install $NvmVersion
+& "$env:LOCALAPPDATA\nvm\nvm.exe" use $NvmVersion
+# Node-gyp needs Python with distutils
+pip install setuptools
