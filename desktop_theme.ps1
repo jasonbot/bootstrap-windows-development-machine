cacdc27f94b3664eeb2f3248131bdb84f4b054db
Jason Scheirer <jscheirer@makenotion.com>
2025-08-12 09:47:11 -0700

    Update desktop_theme.ps1

--- desktop_theme.ps1
+++ desktop_theme.ps1
@@ -12,10 +12,11 @@ Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\C
 # Solid color BG
 Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers" -Name "BackgroundType" -Value 1 -Type "Dword"
 Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" -Name "Wallpaper" -Value "" -Type "String"
-Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Control Panel\Colors" -Name "Background" -Value "249 245 241" -Type "String"
+Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Control Panel\Colors" -Name "Background" -Value "54 129 177" -Type "String"
 
 # Start button on left
 Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Value 0 -Type "Dword"
 
 # Refresh
 Stop-Process -Name explorer -Force; Start-Process explorer
+
