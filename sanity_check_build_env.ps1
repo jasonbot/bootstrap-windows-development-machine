cf647f734d208e67f9520ef4b7ed65b19dde5423
Jason Scheirer <jscheirer@makenotion.com>
2025-08-13 16:38:02 -0700

    More prints

--- sanity_check_build_env.ps1
+++ sanity_check_build_env.ps1
@@ -1,6 +1,8 @@
 # Set up nvm
 $NvmVersion = "v22.13.1"
+Write-Host "Making sure NVM is set up to use $NvmVersion"
 & "$env:LOCALAPPDATA\nvm\nvm.exe" install $NvmVersion
 & "$env:LOCALAPPDATA\nvm\nvm.exe" use $NvmVersion
 # Node-gyp needs Python with distutils
+Write-Host "Node-gyp needsa Python with setuptools installed; modern Pythons don't have it bundled"
 pip install setuptools
--- set_up_winget.ps1
+++ set_up_winget.ps1
@@ -1,8 +1,14 @@
+#Requires -RunAsAdministrator
+
 Write-Host "Making sure Winget works"
 
 $progressPreference = 'silentlyContinue'
+
+Write-Host "   Nuget step"
 Install-PackageProvider -Name NuGet -Force | Out-Null
+Write-Host "   Winget install step"
 Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
+Write-Host "   Winget repair step"
 Repair-WinGetPackageManager -AllUsers
 
 Write-Host "Enabling Dev Mode"
@@ -11,5 +17,4 @@ if (-not(Test-Path -Path $RegistryKeyPath)) {
     New-Item -Path $RegistryKeyPath -ItemType Directory -Force
 }
 
-# Add registry value to enable Developer Mode
 New-ItemProperty -Path $RegistryKeyPath -Name AllowDevelopmentWithoutDevLicense -PropertyType DWORD -Value 1
