0ae3b87b0063279de0cde5be61fb683448a20618
Jason Scheirer <jscheirer@makenotion.com>
2025-08-14 11:10:37 -0700

    Type

--- sanity_check_build_env.ps1
+++ sanity_check_build_env.ps1
@@ -4,5 +4,5 @@ Write-Host "Making sure NVM is set up to use $NvmVersion"
 & "$env:LOCALAPPDATA\nvm\nvm.exe" install $NvmVersion
 & "$env:LOCALAPPDATA\nvm\nvm.exe" use $NvmVersion
 # Node-gyp needs Python with distutils
-Write-Host "Node-gyp needsa Python with setuptools installed; modern Pythons don't have it bundled"
+Write-Host "Node-gyp needs a Python with setuptools installed; modern Pythons don't have it bundled"
 pip install setuptools
