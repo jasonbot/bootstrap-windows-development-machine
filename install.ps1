fa8f321eeda674efb9e080b368e0d9df344ec818
Jason Scheirer <jscheirer@makenotion.com>
2025-09-09 09:52:42 -0700

    So many sanity checks

--- install.ps1
+++ install.ps1
@@ -5,6 +5,25 @@ param (
     [switch]$IWorkHere
 )
 
+function New-Shortcut-To-Powershell-Script-On-Desktop {
+    param (
+        [string]$ScriptPath,
+        [string]$ShortcutName
+    )
+    $desktopPath = [System.Environment]::GetFolderPath('Desktop')
+    $shortcutPath = Join-Path $desktopPath "$ShortcutName.lnk"
+
+    # Create a WScript.Shell object
+    $WshShell = New-Object -comObject WScript.Shell
+
+    $shortcut = $WshShell.CreateShortcut($shortcutPath)
+    $shortcut.TargetPath = "powershell.exe"
+    $shortcut.Arguments = "-NoExit -File `"$ScriptPath`""
+    $shortcut.Save()
+
+    Write-Host "Shortcut '$shortcutName' created on the desktop."
+}
+
 
 Write-Host "Here we go"
 
@@ -12,6 +31,9 @@ Write-Host "Here we go"
 & "$PSScriptRoot\debloat.ps1"
 & "$PSScriptRoot\install_base_apps.ps1"
 & "$PSScriptRoot\desktop_theme.ps1"
+& "$PSScriptRoot\install_base_apps.ps1"
+Start-Process powershell.exe -ArgumentList "-NoExit -File $PSScriptRoot\sanity_check_build_env.ps1"
+New-Shortcut-To-Powershell-Script-On-Desktop -ShortcutName "Setup NVM Etc" -ScriptPath "$PSScriptRoot\sanity_check_build_env.ps1"
 
 if ($IWorkHere) {
     & "$PSScriptRoot\install_fonts.ps1"
